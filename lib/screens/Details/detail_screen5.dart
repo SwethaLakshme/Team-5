6f61f5ed1a90d41e12683bc32db7cd4fda6e7f81
Dharshini1810 <70425084+Dharshini1810@users.noreply.github.com>
2020-10-09 12:32:20 +0530

    added details to patient 5

---
+++ lib/screens/Details/detail_screen5.dart
@@ -0,0 +1,246 @@
+import 'package:prescription_app/components/schedule_card.dart';
+import 'package:prescription_app/constant.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class DetailScreen5 extends StatelessWidget {
+  var _name;
+  var _description;
+  var _imageUrl;
+
+  DetailScreen5(this._name, this._description, this._imageUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/detail_illustration.png'),
+              alignment: Alignment.topCenter,
+              fit: BoxFit.fitWidth,
+            ),
+          ),
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 50,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 30,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    InkWell(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: SvgPicture.asset(
+                        'assets/icons/back.svg',
+                        height: 18,
+                      ),
+                    ),
+                    SvgPicture.asset(
+                      'assets/icons/3dots.svg',
+                      height: 18,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.24,
+              ),
+              Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: kBackgroundColor,
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(50),
+                  ),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(30),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Image.asset(
+                            _imageUrl,
+                            height: 120,
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                _name,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                  color: kTitleTextColor,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                _description,
+                                style: TextStyle(
+                                  color: kTitleTextColor.withOpacity(0.7),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Row(
+                                children: <Widget>[
+                                  Container(
+                                    padding: EdgeInsets.all(10),
+                                    decoration: BoxDecoration(
+                                      color: kBlueColor.withOpacity(0.1),
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                    child: SvgPicture.asset(
+                                      'assets/icons/phone.svg',
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 16,
+                                  ),
+                                  Container(
+                                    padding: EdgeInsets.all(10),
+                                    decoration: BoxDecoration(
+                                      color: kYellowColor.withOpacity(0.1),
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                    child: SvgPicture.asset(
+                                      'assets/icons/chat.svg',
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 16,
+                                  ),
+                                  Container(
+                                    padding: EdgeInsets.all(10),
+                                    decoration: BoxDecoration(
+                                      color: kOrangeColor.withOpacity(0.1),
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                    child: SvgPicture.asset(
+                                      'assets/icons/video.svg',
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+                      SizedBox(
+                        height: 50,
+                      ),
+                      Text(
+                        'Patient Details ',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          color: kTitleTextColor,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'Age: 66 \n Blood Group: B+ve \n Height/Weight: 170cm/62kg \n Blood Pressure(BP): 110mmHg \n Pulse rate: 77beats/min \n Diagonised with: Cardio Vascular Problem',
+                        style: TextStyle(
+                          height: 1.6,
+                          color: kTitleTextColor.withOpacity(0.7),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+
+                      Text(
+                        'Prescribed Medicines',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          color: kTitleTextColor,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'Betacard-10mg ; AF ; 1-1-1 \n Statin-10mg ; BF ; 0-0-1 \n Aspirin-4g ; AF ; 1-0-1 \n Warfarin-10mg ; AF ; 0-0-1 \n Clopidorgel-75mg ; AF ; 1-0-1 ; \n Rivoraxan-50mg ; AF ; 1-0-1',
+                        style: TextStyle(
+                          height: 1.6,
+                          color: kTitleTextColor.withOpacity(0.7),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+
+                      Text(
+                        'Previous Consultations',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          color: kTitleTextColor,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      ScheduleCard(
+                        'Regular Checkup',
+                        'Saturday . 11am - 2pm',
+                        '19',
+                        'Sept',
+                        kBlueColor,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      ScheduleCard(
+                        'Regular Checkup',
+                        'Tuesday . 9am - 11am',
+                        '20',
+                        'Oct',
+                        kYellowColor,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      ScheduleCard(
+                        'Regular Checkup',
+                        'Wednesday . 9am - 11am',
+                        '18',
+                        'Nov',
+                        kOrangeColor,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
